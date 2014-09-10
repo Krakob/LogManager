958d783bf04de00aea90ed843a223c0ec96b0dbc
Jakob Krakob <jakob-s98@hotmail.com>
2014-09-10 12:22:10 +0200

    Functional Excel conversion function
    Also a test log, and various fixes.

--- LogManager.py
+++ LogManager.py
@@ -7,7 +7,7 @@
 ## importing this Python file into another script.
 ##
 ## Spiral Knights forums thread: 
-## GitHub repository: 
+## GitHub repository: https://github.com/jakobs98/LogManager
 ################################
 
 
@@ -20,15 +20,29 @@ import csv
 
 
 ################################
+## Variables
+################################
+header = ['Timestamp', 'Category', 'Name', 'Message']
+
+
+################################
 ## Functions
 ################################
 def read_log(filename): #Returns filename as a list of dicts.
     with open(filename) as f:
         return list(csv.DictReader(f))
-		
+        #TODO: deal with messages of the day which break the CSV,
+        #i.e. contain the sequence of characters ' "," ' or similar.
+
+def format_excel(log_data, filename):
+    with open(filename, 'w', newline='') as f:
+        writer = csv.DictWriter(f, header, delimiter='\t')
+        writer.writeheader()
+        writer.writerows(log_data)
+
 
 ################################
 ## Main code
 ################################
-if __name__ == "__main__":
-    print(read_log("testlog.csv"))
+if __name__ == '__main__':
+    format_excel(read_log('testlog.csv'), 'excelformat.csv')
